20efa457228c05a6993cd32acb5c424c659e8f0b
yv <yashikvlada@gmail.com>
2020-10-31 02:28:39 +0300

    перенес клиент на WinForms

--- exchange_rates_app/client/Form1.cs
+++ exchange_rates_app/client/Form1.cs
@@ -13,39 +13,74 @@ using System.Configuration;
 using System.IO;
 
 namespace client
-{    
+{
     public partial class Form_client : Form
     {
-
+        private ClientSide _client;
+        private IPEndPoint _endPoint;
         public Form_client()
         {
             InitializeComponent();
-            var server_ip = ConfigurationManager.AppSettings["server_ip"].ToString();
-            var server_port = ConfigurationManager.AppSettings["server_port"].ToString();
 
-            //textBox_console.DataBindings.Add("Text", _clientApp, "Log", false, DataSourceUpdateMode.OnPropertyChanged);
+            try
+            {
+                var server_ip = ConfigurationManager.AppSettings["server_ip"].ToString();
+                var server_port = ConfigurationManager.AppSettings["server_port"].ToString();
+
+                _endPoint = new IPEndPoint(IPAddress.Parse(server_ip), int.Parse(server_port));
+
+                _client = new ClientSide();
+                textBox_console.DataBindings.Add("Text", _client, "Log", false, DataSourceUpdateMode.OnPropertyChanged);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Критическая ошибка! [{ex.Message}]");
+                _client?.Close();
+                this.Close();
+            }
 
             this.FormClosed += Form_client_FormClosed;
         }
 
         private void Form_client_FormClosed(object sender, FormClosedEventArgs e)
         {
-
+            _client?.Close();
         }
 
         private void button_connect_Click(object sender, EventArgs e)
         {
+            var login = textBox_login.Text;
+            var pass = textBox_password.Text;
 
+            if (login == null || pass == null)
+            {
+                MessageBox.Show("Enter login and pass pls!");
+                return;
+            }
+            Task.Run(new Action(() => { 
+            _client.Start(_endPoint, login, pass);
+            }));
+  
         }
 
         private void button_ask_Click(object sender, EventArgs e)
         {
-            
+            string curr1 = textBox_curr1.Text;
+            string curr2 = textBox_curr2.Text;
+
+            if (curr1 == null || curr2 == null)
+            {
+                MessageBox.Show("Enter curr1 and curr2 pls!");
+                return;
+            }
+
+            string answ = _client.AskServer(curr1, curr2);
+            textBox_answer.Text += answ + "\r\n";
         }
 
         private void button_disconnect_Click(object sender, EventArgs e)
         {
-
+            _client?.Close();
         }
     }
 }
--- exchange_rates_app/client/client.cs
+++ exchange_rates_app/client/client.cs
@@ -10,15 +10,22 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace client
 {
-  
-    public class Client: INotifyPropertyChanged
+    internal class ClientSide : INotifyPropertyChanged
     {
-
-        private string _log;
         public event PropertyChangedEventHandler PropertyChanged;
+        private string _log;
+
+        private string _userName;
+        private TcpClient _client;
+        private NetworkStream _sw;
+        private StreamReader _sr;
+        private bool _isConnected;
+
+        public bool IsConnected { get => _isConnected; }
         public string Log
         {
             get => _log;
@@ -26,11 +33,98 @@ namespace client
             {
                 if (value != _log)
                 {
-                    _log = value +$" :[{DateTime.Now}]\r\n";
+                    _log = value + $" :[{DateTime.Now}]\r\n";
                     NotifyPropertyChanged();
                 }
             }
         }
+
+        public ClientSide()
+        {
+            
+        }
+        private bool Auth(string login, string pass)
+        {
+            var servAvailble = _sr.ReadLine();
+            Log += servAvailble;
+            if (servAvailble.Contains("Server is not available"))
+                return false;
+
+            var loginBuff = Encoding.Unicode.GetBytes(login + "\r\n");
+            var passBuff = Encoding.Unicode.GetBytes(pass + "\r\n");
+            _sw.Write(loginBuff, 0, loginBuff.Length);
+            _sw.Write(passBuff, 0, passBuff.Length);
+
+            string answ = _sr.ReadLine();
+            Log += answ;
+            if (answ.Contains("Wrong password"))
+                return false;
+
+            answ = _sr.ReadLine();
+            Log += answ;
+            if (answ.Contains("You are in block list"))
+                return false;
+
+            return true;
+        }
+        public void Start(IPEndPoint ep, string login, string pass)
+        {
+            try
+            {
+                _client = new TcpClient();
+                _isConnected = true;
+
+                _userName = login;
+                Log += "Trying to connect...";
+                _client.Connect(ep);
+
+                _sw = _client.GetStream();
+                _sr = new StreamReader(_client.GetStream(), Encoding.Unicode);
+
+                if (!Auth(login, pass))
+                    this.Close();
+            }
+            catch(Exception ex)
+            {
+                Log += ex.Message;
+                this.Close();
+            }
+        }
+        public string AskServer(string curr1, string curr2)
+        {
+            string msgFromServer = "Error";
+            try
+            {
+                byte[] curr1Buff = Encoding.Unicode.GetBytes(curr1 + "\r\n");
+                _sw.Write(curr1Buff, 0, curr1Buff.Length);
+
+                byte[] curr2Buff = Encoding.Unicode.GetBytes(curr2 + "\r\n");
+                _sw.Write(curr2Buff, 0, curr2Buff.Length);
+
+                msgFromServer = _sr.ReadLine();
+                Log += $"Answer from server: {msgFromServer}";
+            }
+            catch(Exception ex)
+            {
+                Log += ex.Message;
+                this.Close();
+            }
+            return msgFromServer;
+        }
+        public void Close()
+        {
+            _isConnected = false;
+            _sr?.Close();
+            _sw?.Close();
+
+            if (_client != null){
+                _client.Close();
+                _client = null;
+                Log += "Disconnected!";
+            }
+            
+        }
+
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
